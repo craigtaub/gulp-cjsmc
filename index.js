702d42802fa2fc9558029cdcf48d51ced6d9e0f6
craigtaub <craig.taub@bbc.co.uk>
2016-04-02 15:43:18 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var through = require("through2");
+var cjsmc = require('cjsmc/lib/buildTemplates');
+
+module.exports = function() {
+  return through.obj(function(file, encoding, callback) {
+    file.contents = new Buffer(String(cjsmc()) + "\n");
+    this.push(file);
+    callback();
+  });
+};
