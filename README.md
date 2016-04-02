7c2262167a92a4d05b64e7b1a72fac72bd946419
craigtaub <craig.taub@bbc.co.uk>
2016-04-02 15:53:12 +0100

    Should export as a module

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # gulp-cjsmc
+
+A gulp-plugin for CJSMC
+
+# Why
+
+CJSMC package details on https://www.npmjs.com/package/cjsmc
+
+# Usage
+
+    var cjsmc = require('gulp-cjsmc');
+    var rename = require('gulp-rename');
+
+    return gulp.src('.')
+      .pipe(cjsmc())
+      .pipe(gulp.dest('client/templates'));
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var through = require("through2");
-var cjsmc = require('cjsmc/lib/buildTemplates');
+var cjsmc = require('../cjsmc/lib/buildTemplates');
 
 module.exports = function() {
   return through.obj(function(file, encoding, callback) {
-    file.contents = new Buffer(String(cjsmc()) + "\n");
+    file.contents = new Buffer(String("module.exports = " + cjsmc()) + "\n");
     this.push(file);
     callback();
   });
